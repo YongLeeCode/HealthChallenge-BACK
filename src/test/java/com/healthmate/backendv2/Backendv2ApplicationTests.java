c38045fdfa5d291ec0c241d56df731c80eceeb6d
YongLeeCode <yonghwan311@gmail.com>
2025-09-16 19:25:05 +0900

    fix: 프로젝트 내용 변경으로 인한 프로젝트 재시작

---
+++ src/test/java/com/healthmate/backendv2/Backendv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthmate.backendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
